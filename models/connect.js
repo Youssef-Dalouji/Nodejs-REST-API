93cc877ebd4ffae88cd5205eafc55fe717f2d753
Youssef Dalouji <daloujiy@gmail.com>
2023-03-15 02:06:57 +0000

    APIII

--- models/connect.js
+++ models/connect.js
@@ -4,10 +4,8 @@ const ConnectDB=async()=>{
     mongoose.set("strictQuery", false)
     try{
         const conn=await mongoose.connect(process.env.DATA_BASE)
-        console.log('MongoDB Connected :'+ conn.connection.host)
     }catch(error){
         console.log(error)
-        process.exit(1)
     }
 }
 module.exports={ConnectDB}
